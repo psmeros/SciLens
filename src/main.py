05ea14ac9c115db9093645db3e2193e83d75c11d
Panayiotis Smeros <psmeros@di.uoa.gr>
2017-02-17 16:13:51 +0100

    Extracted urls from tweets.

--- src/main.py
+++ src/main.py
@@ -16,18 +16,29 @@ sql = '	select body, retweet_count, publishing_date \
 
 
 
-def hasResolvableURL(tweetBody):
-    tweetBody.str.extract("(?P<letter>[a-z])(?P<digit>[0-9])", expand=True)
-    print (tweetBody)
+#def hasResolvableURL(tweets):
+
+def extractURLs(tweets):
+	import re	
+	urlRegex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+	urls = pd.Series(tweets['body'].apply(lambda x: re.findall(urlRegex, x)), name="urls")
+	print(urls)
+
+	tweets = pd.concat([tweets, urls], axis=1)
+
+	return tweets
 
 
 
 tweets = pd.read_sql(sql, con, index_col=None, coerce_float=True, params=None, parse_dates=None, columns=None, chunksize=None)
 
-hasResolvableURL(tweets['body'])
+tweets = extractURLs(tweets)
+
+
 #tweets['hasURL'] = np.where(hasResolvableURL(tweets['body']), True, False)
 
-print (tweets[0:5])
+print (tweets[0:100])
 
 
 
