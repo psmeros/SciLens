19bf8dd853e378b0584175a506f6259fd3f417ac
Panayiotis Smeros <psmeros@di.uoa.gr>
2017-03-03 16:36:41 +0100

    Resolving URLs.

--- src/settings.py
+++ src/settings.py
@@ -6,17 +6,20 @@ pd.set_option('display.width', 1024)
 # Connect to DB
 def connectToDB(user, password, db, host='localhost', port=5432):
     import sqlalchemy
+    import warnings
 
-    '''Returns a connection and a metadata object'''
-    # We connect with the help of the PostgreSQL URL
-    url = 'postgresql://{}:{}@{}:{}/{}'
-    url = url.format(user, password, host, port, db)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=sqlalchemy.exc.SAWarning)
+        '''Returns a connection and a metadata object'''
+        # We connect with the help of the PostgreSQL URL
+        url = 'postgresql://{}:{}@{}:{}/{}'
+        url = url.format(user, password, host, port, db)
 
-    # The return value of create_engine() is our connection object
-    con = sqlalchemy.create_engine(url, client_encoding='utf8')
+        # The return value of create_engine() is our connection object
+        con = sqlalchemy.create_engine(url, client_encoding='utf8')
 
-    # We then bind the connection to MetaData()
-    meta = sqlalchemy.MetaData(bind=con, reflect=True)
+        # We then bind the connection to MetaData()
+        meta = sqlalchemy.MetaData(bind=con, reflect=True)
 
     return con, meta
 
