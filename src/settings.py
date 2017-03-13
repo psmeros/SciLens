490699e778e50d5c6abd19e1e549fbf26af5bcca
Panayiotis Smeros <psmeros@di.uoa.gr>
2017-03-13 21:18:35 +0100

    Plotted # of links per document type.

--- src/settings.py
+++ src/settings.py
@@ -1,16 +1,23 @@
 import pandas as pd
 
-# Global Settings
+#SETTINGS
+#Removes duplicate links from a document
+removeDuplicateLinks = True
+
+# Pandas Settings
 pd.set_option('display.width', 1024)
 
-# Connect to DB
-def connectToDB(user, password, db, host='localhost', port=5432):
+# Pose query to db DB
+def queryDB(query, user, password, db, host='localhost', port=5432):
     import sqlalchemy
     import warnings
 
     with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=sqlalchemy.exc.SAWarning)
         '''Returns a connection and a metadata object'''
+        
+        #ignore warning
+        warnings.simplefilter("ignore", category=sqlalchemy.exc.SAWarning)
+        
         # We connect with the help of the PostgreSQL URL
         url = 'postgresql://{}:{}@{}:{}/{}'
         url = url.format(user, password, host, port, db)
@@ -19,9 +26,9 @@ def connectToDB(user, password, db, host='localhost', port=5432):
         con = sqlalchemy.create_engine(url, client_encoding='utf8')
 
         # We then bind the connection to MetaData()
-        meta = sqlalchemy.MetaData(bind=con, reflect=True)
+        #meta = sqlalchemy.MetaData(bind=con, reflect=True)
 
-    return con, meta
-
-#Removes duplicate links from a document
-removeDuplicateLinks = True
+        #results as pandas dataframe
+        results = pd.read_sql_query(query, con, index_col=None, coerce_float=True, params=None, parse_dates=None, chunksize=None)
+        
+        return results
