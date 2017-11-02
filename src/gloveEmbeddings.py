a6ceb2b74d85391e3eb47ce02de6a62eb1eb0d02
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2017-11-02 21:31:47 +0100

    Various fixes/improvements.

--- src/gloveEmbeddings.py
+++ src/gloveEmbeddings.py
@@ -8,23 +8,31 @@ from sklearn.metrics.pairwise import cosine_similarity
 gloveEmbeddings = gloveEmbeddingsSize = None
 
 #Loads the file.
-def loadGloveEmbeddings(filename):
-    if not os.path.exists(filename):
-        print(filename,'embeddings not found')
+def loadGloveEmbeddings():    
+    try: 
+        gloveFile = os.environ['gloveFile']
+    except: 
+        gloveFile = ('/home/psmeros/var/workspaces/nutrition-workspace/bigFiles/glove.6B.50d.txt' if sys.platform == 'linux' else '/Users/smeros/workspace/etc/bigFiles/glove.6B/glove.6B.50d.txt')
+
+    if not os.path.exists(gloveFile):
+        print(gloveFile,'embeddings not found')
         return None
     words = {} #key= word, value=embeddings
-    with open(filename, "r") as f:
+    with open(gloveFile, "r") as f:
         for line in f:
             tokens = line.strip().split()
             words[tokens[0]] = np.array([float(x) for x in tokens[1:]])
     
-    global gloveEmbeddings, gloveEmbeddingsSize
-    gloveEmbeddings, gloveEmbeddingsSize = words, len(next(iter(words.values())))
+    return words, len(next(iter(words.values())))
 
     
 #Returns the vector of a word.
 def word2vec(word):
     global gloveEmbeddings, gloveEmbeddingsSize
+    
+    if gloveEmbeddingsSize == None:
+        gloveEmbeddings, gloveEmbeddingsSize = loadGloveEmbeddings(gloveFile)
+   
     word = word.lower().strip()
     try:
         return(gloveEmbeddings[word])
@@ -51,8 +59,13 @@ def body2Vec(body):
 
 #Returns the vector of the title of an article.
 def title2Vec(title):
+    return sent2Vec(title)   
+
+
+#Returns the vector of a sentence.
+def sent2Vec(sentence):
     vec = word2vec('')
-    for w in wordpunct_tokenize(title):
+    for w in wordpunct_tokenize(sentence):
          if len(w)!=1 and w not in stopWords:
             vec += word2vec(w)
-    return vec       
+    return vec   
