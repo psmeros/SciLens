b445b82db2f68bf25663894c534d141422329a3e
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2017-12-05 13:26:11 +0100

    Added arrow option.

--- src/utils.py
+++ src/utils.py
@@ -16,6 +16,7 @@ def initSpark():
     conf = SparkConf()
     conf.setAppName('quoteAnalysis')
     conf.setMaster('local[*]')
+    conf.set('spark.sql.execution.arrow.enable', 'true')
     conf.set('spark.executor.memory', memory)
     conf.set('spark.driver.memory', memory)
     conf.set('spark.driver.maxResultSize', '40G')
