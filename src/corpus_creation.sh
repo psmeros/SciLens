cd1b2005314ae84491990c62f914a9d15ffd41ee
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2018-02-02 20:31:58 +0100

    Added a script for corpus creation.

---
+++ src/corpus_creation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+user='smeros'
+db='sciArticles'
+
+limit='limit 100'
+tmpfolder='/tmp/'
+webfile='webFood.tsv'
+twitterfile='twitterFood.tsv'
+
+rm -f $tmpfolder$webfile
+psql -U $user -d $db -c "\copy (select url, (title || '. ' || regexp_replace(regexp_replace(body, E'[\\n\\r]+', ' ', 'g' ), '([^[:ascii:]])', '', 'g')) as article, publishing_date from document where doc_type = 'web' $limit ) TO $tmpfolder$webfile;"
+mv $tmpfolder$webfile .
+
+rm -f $tmpfolder$twitterfile
+psql -U $user -d $db -c "\copy (select url, body as tweet, publishing_date, array_to_string(document_urls, ',', '') from document where doc_type = 'twitter' $limit ) TO $tmpfolder$twitterfile;"
+mv $tmpfolder$twitterfile .
+
