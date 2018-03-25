9273ae46de5129958eb2091e505d10f97ece1818
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2018-03-26 01:16:56 +0200

    Started converting files to networkX graph.

--- src/run.py
+++ src/run.py
@@ -16,6 +16,25 @@ t0 = time()
 
 #first_level_graph()
 
-second_level_graph()
+#second_level_graph()
+
+#create graph
+if not useCache or not os.path.exists(first_level_graph_file):
+	first_level_graph()
+
+df = pd.read_csv(first_level_graph_file, sep='\t')
+
+G = nx.from_pandas_dataframe(df, 'tweet_url', 'out_url')
+
+df = df.drop_duplicates('tweet_url').set_index('tweet_url')
+
+for attr in ['timestamp', 'popularity', 'RTs', 'user_country']:
+	nx.set_node_attributes(G, df[attr].to_dict(), attr)
+
+
+#print(df['timestamp'])
+print(G.nodes(data=True))
+
+
 
 print("Total time: %0.3fs." % (time() - t0))
