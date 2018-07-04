cd02f538236728d6cb2d68240b495db8fd5cf2e8
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2018-07-04 21:19:26 +0200

    Postgres dump script update.

--- src/bin/corpus_creation.sh
+++ src/bin/corpus_creation.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-user='smeros'
-db='sciArticles'
+user=''
+db=''
+host=''
 
-limit='limit 100'
+limit=''
 tmpfolder='/tmp/'
 webfile='webFood.tsv'
 twitterfile='twitterFood.tsv'
 
 rm -f $tmpfolder$webfile
-psql -U $user -d $db -c "\copy (select url, (title || '. ' || regexp_replace(regexp_replace(body, E'[\\n\\r]+', ' ', 'g' ), '([^[:ascii:]])', '', 'g')) as article, publishing_date from document where doc_type = 'web' $limit ) TO $tmpfolder$webfile;"
+psql -U $user -d $db -h $host -W -c "\copy (select url, title, (regexp_replace(regexp_replace(body, E'[\\n\\r]+', ' ', 'g' ), '([^[:ascii:]])', '', 'g')) as body, publishing_date from document where doc_type = 'web' $limit ) TO $tmpfolder$webfile;"
 mv $tmpfolder$webfile .
 
 rm -f $tmpfolder$twitterfile
-psql -U $user -d $db -c "\copy (select url, body as tweet, publishing_date, base_popularity, retweet_count, user_country from document where doc_type = 'twitter' $limit ) TO $tmpfolder$twitterfile;"
+psql -U $user -d $db -h $host -c "\copy (select url, body as tweet, publishing_date, base_popularity, retweet_count, user_country from document where doc_type = 'twitter' $limit ) TO $tmpfolder$twitterfile;"
 mv $tmpfolder$twitterfile .
-
-
-##Helpers##
-
-#extract remote postgres dump
-#export PGPASSWORD=<PGPASSWORD>; pg_dump -h <HOST> -U <USER> -f <DBNAME>.db <DBNAME>
-
-#load dump to local postgres
-#psql <DBNAME> < <DBNAME>.db
-
-#sample .tsv file
-#shuf -n 10000 file.tsv > sampleFile.tsv
