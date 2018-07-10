f738857395a2f82f2ea40d3d924db0e05e473b64
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2018-07-10 20:23:27 +0200

    Added function that prunes the diffusion graph.

--- src/graph_ops.py
+++ src/graph_ops.py
@@ -5,8 +5,40 @@ from diffusion_graph import create_graph
 from settings import *
 from url_helpers import analyze_url
 
+#prune the initial diffusion graph by keeping only the paths that contain the selected papers
+def prune_graph(papersFile):
 
+    if not useCache or not os.path.exists(diffusion_graph_dir+'pruned_graph.tsv'):
+        G = create_graph()
 
+        df = pd.read_csv(diffusion_graph_dir+'epoch_0.tsv', sep='\t').dropna()
+        df['social'] = project_url+'#twitter'
+        G =  nx.compose(G, nx.from_pandas_edgelist(df, source='social', target='source_url', create_using=nx.DiGraph()))
+
+        papers = open(papersFile).read().splitlines()
+
+        newG = nx.DiGraph()
+        for path in nx.all_simple_paths(G, source=project_url+'#twitter', target=project_url+'#source'):
+            for paper in papers:
+                if paper in path:
+                    for i in range(1, len(path)):
+                        newG.add_edge(path[i-1], path[i])
+
+        print(len([s for s in newG.successors(project_url+'#twitter')]), 'tweets out of', len(newG.nodes), 'nodes')
+    
+        with open(diffusion_graph_dir+'pruned_graph.tsv', 'w') as f:
+            for edge in newG.edges:
+                    f.write(edge[0] + '\t' + edge[1] + '\n')
+
+    G = nx.DiGraph()
+    edges = open(diffusion_graph_dir+'pruned_graph.tsv').read().splitlines()
+    for e in edges:
+        [e0, e1] = e.split()
+        G.add_edge(e0, e1)
+
+    return G
+
+#get selected papers
 def get_most_widely_referenced_publications(different_domains, filename):
     G = create_graph()
 
