df24cb73027b787f157791d7a93d94bd895af051
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2018-07-18 03:56:41 -0700

    Extracted quote indicators.

--- src/run.py
+++ src/run.py
@@ -1,9 +1,11 @@
 from time import time
 from diffusion_graph import create_graph
 from graph_ops import download_articles, get_effective_documents, prune_graph
+from quote_extraction import extract_quotes
 
 t0 = time()
 
-get_effective_documents('pruned_graph_v2.tsv', 'cache/top_paper_3_tweets.txt', 'tweets')
+extract_quotes('cache/article_details_small.tsv', 'cache/article_details_2.tsv')
+
 
 print("Total time: %0.3fs." % (time() - t0))
