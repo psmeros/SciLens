2dd68f96a8e37119dd20aa85f1c399658797b6ac
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2018-07-23 03:26:33 +0200

    First try for computing faithfulness.

--- src/glove.py
+++ src/glove.py
@@ -9,39 +9,29 @@ from settings import *
 
 nlp = spacy.load('en')
 
-#GloVe Embeddings file
-gloveFile = None
-
-gloveEmbeddings = gloveEmbeddingsSize = None
-
-if not os.path.exists(gloveFile):
-    print(gloveFile,'embeddings not found!')
-    sys.exit(0)
 
 #Load GloVe file
-def loadGloveEmbeddings():    
+def load_Glove():    
     words = {}
-    with open(gloveFile, "r") as f:
+    with open(glove_file, "r") as f:
         for line in f:
             tokens = line.strip().split()
             words[tokens[0]] = np.array([float(x) for x in tokens[1:]])
   
-    global gloveEmbeddings, gloveEmbeddingsSize
-    gloveEmbeddings, gloveEmbeddingsSize = words, len(next(iter(words.values())))
+    return words, len(next(iter(words.values())))
+
+glove_embeddings, glove_embeddings_size = load_Glove() 
 
 #Return the vector of a word
 def word2vec(word):
-    global gloveEmbeddings, gloveEmbeddingsSize        
-    if gloveEmbeddingsSize == None: loadGloveEmbeddings()
-    
     try:
-        return gloveEmbeddings[word.lower().strip()]
+        return glove_embeddings[word.lower().strip()]
     except:
-        return np.zeros(gloveEmbeddingsSize)
+        return np.zeros(glove_embeddings_size)
 
     
 #Return the vector of a sentence
-def sent2Vec(sentence):    
+def sent2vec(sentence):    
     vec = word2vec('')
     for w in nlp(sentence):
          if not (w.is_stop or w.is_punct):
@@ -52,10 +42,10 @@ def sent2Vec(sentence):
 def topics2Vec(topics):
     tEmbeddings = {}
     for t in topics:
-        tEmbeddings[t] = sent2Vec(' '.join(t.split(',')))
+        tEmbeddings[t] = sent2vec(' '.join(t.split(',')))
     return tEmbeddings
 
 
 #Compute the cosine similarity between two vectors
-def sim(vec1, vec2):
+def cos_sim(vec1, vec2):
     return abs(cosine_similarity(vec1.reshape(1, -1), vec2.reshape(1, -1))[0][0])
