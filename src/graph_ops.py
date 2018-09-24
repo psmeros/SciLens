cdb66b953d04423c5c67d495a61cf0221a88ee45
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2018-09-24 19:28:42 +0200

    Prepared for crowdsourcing annotation.

--- src/graph_ops.py
+++ src/graph_ops.py
@@ -43,59 +43,13 @@ def remove_duplicate_text(article_in_file, graph_in_file, article_out_file, grap
     article_details.to_csv(article_out_file, sep='\t', index=None)
 
 
-
-def aggregate_tweet_details(graph_file, tweet_file, article_in_file, article_out_file):
-    G = read_graph(graph_file)
-    tweet_details = pd.read_csv(tweet_file, sep='\t')
-    article_details = pd.read_csv(article_in_file, sep='\t')
-    
-    def func(url, tweet_details):
-        tweet_details = tweet_details.copy()
-        if len(tweet_details['publish_date']) in [0, 1]:
-            delta_in_hours = 0
-        else:
-            tweet_details['publish_date'] = pd.to_datetime(tweet_details['publish_date']).astype('int64')//1e9
-            [dmin, dmax] = np.percentile(tweet_details['publish_date'].tolist(), [5, 95])
-            delta_in_hours = (dmax-dmin) // 3600
-            if len(tweet_details['publish_date'])!=2:
-                tweet_details = tweet_details[(dmin<tweet_details['publish_date']) & (tweet_details['publish_date']<dmax)]
-    
-        agg = [url]
-        agg.append(delta_in_hours)
-        agg.append(len(tweet_details['user_country'].dropna().unique().tolist()))
-        agg = agg + tweet_details[['RTs', 'replies_num', 'likes']].sum(axis=0).tolist()
-        agg = agg + tweet_details[['tweet_polarity', 'tweet_subjectivity', 'replies_mean_polarity', 'replies_mean_subjectivity']].mean(axis=0).tolist()
-        agg = agg + tweet_details[['user_followers_count', 'user_tweet_count', 'user_friends_count']].median(axis=0).tolist()
-
-        agg = {'url':agg[0], 'tweets_time_delta':agg[1], 'users_countries':agg[2], 'retweets':agg[3], 'replies':agg[4], 'likes':agg[5], 'tweets_mean_polarity':agg[6], 'tweets_mean_subjectivity':agg[7], 'replies_mean_polarity':agg[8], 'replies_mean_subjectivity':agg[9], 'users_median_followers':agg[10], 'users_median_tweets':agg[11], 'users_median_friends':agg[12]}
-        return agg
-
-    article_details = article_details.merge(pd.DataFrame(article_details['url'].apply(lambda x: func(x, tweet_details[tweet_details['url'].isin(G.predecessors(x))])).tolist()), on='url')
-    article_details.to_csv(article_out_file, sep='\t', index=None)
-
-
-#extend tweet file
-def extent_tweets(in_file, out_file):
-    tweet_details = pd.read_csv(in_file, sep='\t')
-    tweet_details['likes'] = tweet_details['popularity'] - tweet_details['RTs']
-    tweet_details['tweet_polarity'] = tweet_details['full_text'].apply(lambda x: TextBlob(x).sentiment.polarity)
-    tweet_details['tweet_subjectivity'] = tweet_details['full_text'].apply(lambda x: TextBlob(x).sentiment.subjectivity)
-    tweet_details['replies_mean_polarity'] = tweet_details['replies'].apply(lambda x: np.mean([TextBlob(r).sentiment.polarity for r in eval(x)]))
-    tweet_details['replies_mean_subjectivity'] = tweet_details['replies'].apply(lambda x: np.mean([TextBlob(r).sentiment.subjectivity for r in eval(x)]))
-    tweet_details['user'] = tweet_details['url'].apply(lambda x: x.split('/')[3])
-    tweet_details = tweet_details.merge(pd.read_csv(twitter_users_file, sep='\t'), left_on='user', right_on='screen_name', how='left')
-    tweet_details = tweet_details.drop(['popularity', 'user', 'screen_name'], axis=1)
-    tweet_details = tweet_details.replace('\\N',np.NaN)
-    tweet_details.to_csv(out_file, sep='\t', index=None)
-
-
 #get scientific - news article (true and false) pairs
-def get_article_pairs(graph_file, articles_file, pairs_out_file, multiple_links=False):
+def get_article_pairs(graph_file, articles_file, pairs_out_file, set_type):
 
     G = read_graph(graph_file)
     articles = pd.read_csv(articles_file, sep='\t')['url'].tolist()
 
-    if multiple_links == True:
+    if set_type == 'test':
         pairs = []
         for a in articles:
             if G.out_degree(a) > 1:
@@ -105,7 +59,7 @@ def get_article_pairs(graph_file, articles_file, pairs_out_file, multiple_links=
         df = pd.DataFrame(pairs, columns=['article', 'paper'])
         df.to_csv(pairs_out_file, sep='\t', index=None)
 
-    else:
+    elif set_type == 'train':
         true_pairs = []
         for a in articles:
             if G.out_degree(a) == 1:
@@ -124,19 +78,6 @@ def get_article_pairs(graph_file, articles_file, pairs_out_file, multiple_links=
         df = pd.DataFrame(true_pairs+false_pairs, columns=['article', 'paper', 'related'])
         df.to_csv(pairs_out_file, sep='\t', index=None)
 
-
-#write tweets to file
-def download_tweets(corpus_file, in_file, out_file, sleep_time):
-
-    df = pd.read_csv(twitterCorpusFile, sep='\t', header=None)
-    df[0] = df[0].apply(lambda x: x.replace('https://','http://'))
-    tweets_details = pd.read_csv(in_file, sep='\t', header=None)
-    tweets_details = tweets_details.merge(df, left_on=0, right_on=0)
-    tweets_details.columns = ['url','full_text','publish_date','popularity','RTs','user_country']
-    
-    tweets_details['replies'] = tweets_details['url'].apply(lambda x: scrap_twitter_replies(x, sleep_time))
-    tweets_details['replies_num'] = tweets_details['replies'].apply(lambda x: len(x))
-    tweets_details.to_csv(out_file, sep='\t', index=None)
 
 #write documents to file
 def get_effective_documents(graph_file, out_file, doc_type):
--- src/run.py
+++ src/run.py
@@ -1,9 +1,11 @@
 from time import time
-from settings import *
+
 from graph_ops import *
 from matching import *
-from topic_detection import *
 from quote_extraction import *
+from settings import *
+from topic_detection import *
+from tweets_ops import *
 
 t0 = time()
 
@@ -13,10 +15,14 @@ t0 = time()
 #extent_tweets(cache_dir+'tweet_details_v1.tsv', cache_dir+'tweet_details_v2.tsv')
 #aggregate_tweet_details(cache_dir+'diffusion_graph/pruned_graph_v3.tsv', cache_dir+'tweet_details_v2.tsv', cache_dir+'article_details_v5.tsv', cache_dir+'article_details_v6.tsv')
 #get_article_pairs(cache_dir+'diffusion_graph/pruned_graph_v3.tsv', cache_dir+'article_details_v5.tsv', cache_dir+'pairs.tsv', True)
-compute_pairs_similarity(cache_dir + 'similarity_model_test_pairs_v1.tsv', cache_dir + 'article_details_v5.tsv', cache_dir + 'paper_details_v3.tsv', 'full_text', cache_dir + 'similarity_model_pairs_test_v2_full.tsv')
-compute_pairs_similarity(cache_dir + 'similarity_model_test_pairs_v1.tsv', cache_dir + 'article_details_v5.tsv', cache_dir + 'paper_details_v3.tsv', 'paragraph', cache_dir + 'similarity_model_pairs_test_v2_paragraph.tsv')
-compute_pairs_similarity(cache_dir + 'similarity_model_test_pairs_v1.tsv', cache_dir + 'article_details_v5.tsv', cache_dir + 'paper_details_v3.tsv', 'sentence', cache_dir + 'similarity_model_pairs_test_v2_sentence.tsv')
+#compute_pairs_similarity(cache_dir + 'similarity_model_test_pairs_v1.tsv', cache_dir + 'article_details_v5.tsv', cache_dir + 'paper_details_v3.tsv', 'full_text', cache_dir + 'similarity_model_pairs_test_v2_full.tsv')
+#compute_pairs_similarity(cache_dir + 'similarity_model_test_pairs_v1.tsv', cache_dir + 'article_details_v5.tsv', cache_dir + 'paper_details_v3.tsv', 'paragraph', cache_dir + 'similarity_model_pairs_test_v2_paragraph.tsv')
+#compute_pairs_similarity(cache_dir + 'similarity_model_test_pairs_v1.tsv', cache_dir + 'article_details_v5.tsv', cache_dir + 'paper_details_v3.tsv', 'sentence', cache_dir + 'similarity_model_pairs_test_v2_sentence.tsv')
 #compute_similarity_model(cache_dir+'similarity_model_pairs_v2', cache_dir+'rf_model.sav', False)
 #extract_quotes(cache_dir+'article_details_v3.tsv', cache_dir+'article_details_v4.tsv')
 #remove_duplicate_text(cache_dir+'article_details_v4.tsv', cache_dir+'diffusion_graph/pruned_graph_v2.tsv', cache_dir+'article_details_v5.tsv', cache_dir+'diffusion_graph/pruned_graph_v3.tsv') 
+#test_similarity_model(cache_dir+'similarity_model/test_pairs_v2', cache_dir+'similarity_model/rf_model.sav', cache_dir+'similarity_model/test_pairs_v3.tsv')
+
+prepare_annotation(cache_dir+'tweet_details_v2.tsv', cache_dir+'tweet_replies.tsv')
+
 print("Total time: %0.3fs." % (time() - t0))
