020e85bf67c8a3347ea53fee351ce952b2c94ce2
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2019-06-18 22:47:27 +0200

    Refactoring.

--- src/run.py
+++ src/run.py
@@ -1,29 +1,10 @@
 from time import time
+from pathlib import Path
 
-#from graph_ops import *
-#from matching import *
-#from quote_extraction import *
-#from settings import *
-#from topic_detection import *
-from tweets_ops import *
-from create_corpus import *
+#Scilens Directory
+scilens_dir = str(Path.home()) + '/Dropbox/scilens/'
+
+
 t0 = time()
-
-#train_topic_model(cache_dir + 'article_details_v2.tsv', cache_dir + 'paper_details_v2.tsv', cache_dir + 'topic_model')
-#extent_tweets(cache_dir+'tweet_details_v1.tsv', cache_dir+'tweet_details_v2.tsv')
-#aggregate_tweet_details(cache_dir+'diffusion_graph/pruned_graph_v3.tsv', cache_dir+'tweet_details_v2.tsv', cache_dir+'article_details_v5.tsv', cache_dir+'article_details_v6.tsv')
-#get_article_pairs(cache_dir+'diffusion_graph/pruned_graph_v3.tsv', cache_dir+'article_details_v5.tsv', cache_dir+'pairs.tsv', True)
-#compute_pairs_similarity(cache_dir + 'similarity_model_test_pairs_v1.tsv', cache_dir + 'article_details_v5.tsv', cache_dir + 'paper_details_v3.tsv', 'full_text', cache_dir + 'similarity_model_pairs_test_v2_full.tsv')
-#compute_pairs_similarity(cache_dir + 'similarity_model_test_pairs_v1.tsv', cache_dir + 'article_details_v5.tsv', cache_dir + 'paper_details_v3.tsv', 'paragraph', cache_dir + 'similarity_model_pairs_test_v2_paragraph.tsv')
-#compute_pairs_similarity(cache_dir + 'similarity_model_test_pairs_v1.tsv', cache_dir + 'article_details_v5.tsv', cache_dir + 'paper_details_v3.tsv', 'sentence', cache_dir + 'similarity_model_pairs_test_v2_sentence.tsv')
-#compute_similarity_model(cache_dir+'similarity_model/train_pairs_v2', 'RF', cache_dir+'similarity_model/rf_model.sav', True)
-#extract_quotes(cache_dir+'article_details_v3.tsv', cache_dir+'article_details_v4.tsv')
-#test_similarity_model(cache_dir+'similarity_model/test_pairs_v2', cache_dir+'similarity_model/rf_model.sav', cache_dir+'similarity_model/test_pairs_v3.tsv')
-#prepare_annotation(cache_dir+'tweet_details_v2.tsv', cache_dir+'tweet_replies.tsv')
-
-
-#create_corpus(scilens_dir + 'corpus/scilens_3M.tsv')
-#train_stance_classifier()
-#extent_tweet_details(scilens_dir+'cache/tweet_details_v1.tsv', scilens_dir+'cache/tweet_details_v2.tsv')
 
 print("Total time: %0.3fs." % (time() - t0))
--- src/tweets_ops.py
+++ src/tweets_ops.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import KFold
 from textblob import TextBlob
 
 from create_corpus import read_graph
-from settings import scilens_dir
+from run import scilens_dir
 
 ############################### CONSTANTS ###############################
 
@@ -48,60 +48,6 @@ def stance_features_extraction(df):
     df = df.join(df['Tweet'].apply(lambda x: sid.polarity_scores(x)).apply(pd.Series))
 
     return df
-
-################################ ####### ################################
-
-def aggregate_tweet_details(graph_file, tweet_file, article_in_file, article_out_file):
-    G = read_graph(graph_file)
-    tweet_details = pd.read_csv(tweet_file, sep='\t').fillna(0)
-    article_details = pd.read_csv(article_in_file, sep='\t')
-    
-    def func(url, tweet_details):
-        tweet_details = tweet_details.copy()
-        if len(tweet_details['publish_date']) in [0, 1]:
-            delta_in_hours = 0
-        else:
-            tweet_details['publish_date'] = pd.to_datetime(tweet_details['publish_date']).astype('int64')//1e9
-            [dmin, dmax] = np.percentile(tweet_details['publish_date'].tolist(), [5, 95])
-            delta_in_hours = (dmax-dmin) // 3600
-            if len(tweet_details['publish_date'])!=2:
-                tweet_details = tweet_details[(dmin<tweet_details['publish_date']) & (tweet_details['publish_date']<dmax)]
-    
-        agg = [url]
-        agg.append(delta_in_hours)
-        agg.append(len(tweet_details['user_country'].dropna().unique().tolist()))
-        agg = agg + tweet_details[['RTs', 'replies_num', 'likes']].sum(axis=0).tolist()
-        agg = agg + tweet_details[['tweet_polarity', 'tweet_subjectivity', 'replies_mean_polarity', 'replies_mean_subjectivity']].mean(axis=0).tolist()
-        agg = agg + tweet_details[['user_followers_count', 'user_tweet_count', 'user_friends_count']].median(axis=0).tolist()
-        agg.append(tweet_details['stance'].mean(axis=0))
-
-        agg = {'url':agg[0], 'tweets_time_delta':agg[1], 'users_countries':agg[2], 'retweets':agg[3], 'replies_count':agg[4], 'likes':agg[5], 'tweets_mean_polarity':agg[6], 'tweets_mean_subjectivity':agg[7], 'replies_mean_polarity':agg[8], 'replies_mean_subjectivity':agg[9], 'users_median_followers':agg[10], 'users_median_tweets':agg[11], 'users_median_friends':agg[12], 'stance':agg[13]}
-        return agg
-
-    article_details = article_details.merge(pd.DataFrame(article_details['url'].apply(lambda x: func(x, tweet_details[tweet_details['url'].isin(G.predecessors(x))])).tolist()), on='url')
-    article_details.to_csv(article_out_file, sep='\t', index=None)
-
-
-#extend tweet details with tweets' and replies' sentiment, user features and replies' stance
-def extent_tweet_details(tweets_in_file, tweets_out_file):
-    tweet_details = pd.read_csv(tweets_in_file, sep='\t')
-    tweet_details['likes'] = tweet_details['popularity'] - tweet_details['RTs']
-    tweet_details['tweet_polarity'] = tweet_details['full_text'].apply(lambda x: TextBlob(x).sentiment.polarity)
-    tweet_details['tweet_subjectivity'] = tweet_details['full_text'].apply(lambda x: TextBlob(x).sentiment.subjectivity)
-    tweet_details['replies_mean_polarity'] = tweet_details['replies'].apply(lambda x: np.mean([TextBlob(r).sentiment.polarity for r in eval(x)]))
-    tweet_details['replies_mean_subjectivity'] = tweet_details['replies'].apply(lambda x: np.mean([TextBlob(r).sentiment.subjectivity for r in eval(x)]))
-    tweet_details['user'] = tweet_details['url'].apply(lambda x: x.split('/')[3])
-    tweet_details = tweet_details.merge(pd.read_csv(twitter_users_file, sep='\t'), left_on='user', right_on='screen_name', how='left')
-    tweet_details = tweet_details.drop(['popularity', 'user', 'screen_name'], axis=1)
-    tweet_details = tweet_details.replace('\\N',np.NaN)
-
-    classifier = pickle.load(open(stance_classifier, 'rb'))
-    X = np.array(stance_features_extraction(tweet_details[['replies']].rename(columns={'replies':'Tweet'})).drop('Tweet', axis=1).values, dtype=np.float32)
-    tweet_details['stance']=classifier.predict_proba(X)[:,1]
-    tweet_details['stance']= tweet_details.apply(lambda x: 0 if x.replies_num==0 else x.stance, axis=1)
-       
-    tweet_details.to_csv(tweets_out_file, sep='\t', index=None)
-
 
 #train stance classifier based on semeval and crowd-annotated dataset
 def train_stance_classifier():
@@ -149,3 +95,54 @@ def train_stance_classifier():
         classifier = RandomForestClassifier(n_estimators=n_est, max_depth=m_dep, n_jobs=-1)
         classifier.fit(X, y)
         pickle.dump(classifier, open(stance_classifier, 'wb'))    
+
+################################ ####### ################################
+
+#attach social media details to article like tweet and replies sentiment, user features and replies' stance
+def attach_social_media_details(graph_file, tweet_file, article_file):
+    G = read_graph(graph_file)
+
+    
+    tweet_details = pd.read_csv(tweet_file, sep='\t')
+    tweet_details['likes'] = tweet_details['popularity'] - tweet_details['RTs']
+    tweet_details['tweet_polarity'] = tweet_details['full_text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+    tweet_details['tweet_subjectivity'] = tweet_details['full_text'].apply(lambda x: TextBlob(x).sentiment.subjectivity)
+    tweet_details['replies_mean_polarity'] = tweet_details['replies'].apply(lambda x: np.mean([TextBlob(r).sentiment.polarity for r in eval(x)]))
+    tweet_details['replies_mean_subjectivity'] = tweet_details['replies'].apply(lambda x: np.mean([TextBlob(r).sentiment.subjectivity for r in eval(x)]))
+    tweet_details['user'] = tweet_details['url'].apply(lambda x: x.split('/')[3])
+    tweet_details = tweet_details.merge(pd.read_csv(twitter_users_file, sep='\t'), left_on='user', right_on='screen_name', how='left')
+    tweet_details = tweet_details.drop(['popularity', 'user', 'screen_name'], axis=1)
+    tweet_details = tweet_details.replace('\\N',np.NaN)
+
+    classifier = pickle.load(open(stance_classifier, 'rb'))
+    X = np.array(stance_features_extraction(tweet_details[['replies']].rename(columns={'replies':'Tweet'})).drop('Tweet', axis=1).values, dtype=np.float32)
+    tweet_details['stance']=classifier.predict_proba(X)[:,1]
+    tweet_details['stance']= tweet_details.apply(lambda x: 0 if x.replies_num==0 else x.stance, axis=1)
+    tweet_details = tweet_details.fillna(0)
+
+    article_details = pd.read_csv(article_file, sep='\t')
+    
+    def func(url, tweet_details):
+        tweet_details = tweet_details.copy()
+        if len(tweet_details['publish_date']) in [0, 1]:
+            delta_in_hours = 0
+        else:
+            tweet_details['publish_date'] = pd.to_datetime(tweet_details['publish_date']).astype('int64')//1e9
+            [dmin, dmax] = np.percentile(tweet_details['publish_date'].tolist(), [5, 95])
+            delta_in_hours = (dmax-dmin) // 3600
+            if len(tweet_details['publish_date'])!=2:
+                tweet_details = tweet_details[(dmin<tweet_details['publish_date']) & (tweet_details['publish_date']<dmax)]
+    
+        agg = [url]
+        agg.append(delta_in_hours)
+        agg.append(len(tweet_details['user_country'].dropna().unique().tolist()))
+        agg = agg + tweet_details[['RTs', 'replies_num', 'likes']].sum(axis=0).tolist()
+        agg = agg + tweet_details[['tweet_polarity', 'tweet_subjectivity', 'replies_mean_polarity', 'replies_mean_subjectivity']].mean(axis=0).tolist()
+        agg = agg + tweet_details[['user_followers_count', 'user_tweet_count', 'user_friends_count']].median(axis=0).tolist()
+        agg.append(tweet_details['stance'].mean(axis=0))
+
+        agg = {'url':agg[0], 'tweets_time_delta':agg[1], 'users_countries':agg[2], 'retweets':agg[3], 'replies_count':agg[4], 'likes':agg[5], 'tweets_mean_polarity':agg[6], 'tweets_mean_subjectivity':agg[7], 'replies_mean_polarity':agg[8], 'replies_mean_subjectivity':agg[9], 'users_median_followers':agg[10], 'users_median_tweets':agg[11], 'users_median_friends':agg[12], 'stance':agg[13]}
+        return agg
+
+    article_details = article_details.merge(pd.DataFrame(article_details['url'].apply(lambda x: func(x, tweet_details[tweet_details['url'].isin(G.predecessors(x))])).tolist()), on='url')
+    return article_details
