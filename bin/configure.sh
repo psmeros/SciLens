09a5f3fefd56364502cb4b424477f4c86b705182
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2019-08-05 20:31:23 +0200

    Update configure.sh

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,21 +1,20 @@
 
 #!/bin/sh
 
-#sudo apt-get update
-#sudo apt-get upgrade -y
-#sudo apt-get install -y openjdk-8-jre screen htop git vim
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y openjdk-8-jre screen htop git vim
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh
-rm Miniconda3-latest-Linux-x86_64.sh
 
 source ~/.bashrc
 
 conda install -y pandas numpy networkx nltk spacy pyspark beautifulsoup4 scikit-learn
-pip install -U textblob newspaper3k textstat
+conda install pytorch cudatoolkit=9.0 -c pytorch
+pip install -U newspaper3k textstat scispacy https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.0/en_core_sci_md-0.2.0.tar.gz
 python -m nltk.downloader punkt vader_lexicon #-d /path/to/nltk_data
-python -m spacy download en
-python -m textblob.download_corpora
+python -m spacy download en_core_web_lg 
 
-#gloVe 6B (Wikipedia - 300d) [deprecated]
+rm Miniconda3-latest-Linux-x86_64.sh
