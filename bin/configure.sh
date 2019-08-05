a7e20fbdab3574a7abb3f04b1443dabcf97f77f8
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2019-08-06 01:31:54 +0200

    Update configure.sh

--- bin/configure.sh
+++ bin/configure.sh
@@ -13,7 +13,7 @@ source ~/.bashrc
 
 conda install -y pandas numpy networkx nltk spacy pyspark beautifulsoup4 scikit-learn
 conda install pytorch cudatoolkit=9.0 -c pytorch
-pip install -U newspaper3k textstat scispacy https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.0/en_core_sci_md-0.2.0.tar.gz
+pip install -U newspaper3k textstat scispacy https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.0/en_core_sci_md-0.2.0.tar.gz pandarallel
 python -m nltk.downloader punkt vader_lexicon #-d /path/to/nltk_data
 python -m spacy download en_core_web_lg 
 
